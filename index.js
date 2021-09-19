5482d464e554cd6c0735cf70f04922795b17f5b0
David Morgenstern <DavidtheConfident@gmail.com>
2021-09-19 17:06:16 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ function preserveDecimal(string)
 {
     return parseFloat(string);
 }
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(b, a){
+    return b - a
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(b, a){
+    return b / a
+}
+
+function increment(a){
+    return ++a
+}
+
+function decrement(a){
+    return --a
+}
+
+
